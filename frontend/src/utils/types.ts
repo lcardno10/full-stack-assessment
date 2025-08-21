b2bb7bd42fd27ee7fa18e351f7a1a8dbd6229a79
Lewis Card <lcard@no10.gov.uk>
2025-08-21 16:42:25 +0100

    Initial vibe coded frontend

--- frontend/src/utils/types.ts
+++ frontend/src/utils/types.ts
@@ -1,7 +1,26 @@
 export interface GapminderData {
     country: string
+    continent: string
     year: number
     lifeExp: number
     gdpPercap: number
     pop: number
+  }
+
+export interface FilterState {
+    selectedCountries: string[]
+    selectedContinents: string[]
+    yearRange: [number, number]
+    gdpRange: [number, number]
+    lifeExpRange: [number, number]
+    popRange: [number, number]
+  }
+
+export interface ChartConfig {
+    xAxis: 'gdpPercap' | 'year' | 'pop'
+    yAxis: 'lifeExp' | 'gdpPercap' | 'pop'
+    sizeBy: 'pop' | 'gdpPercap' | 'static'
+    colorBy: 'continent' | 'country'
+    showTrendLines: boolean
+    logScale: boolean
   }
